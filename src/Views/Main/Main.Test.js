43039e841d254d0e4087e09ab905f74588ea95f5
Joshua Stresing <pyrotorch69@yahoo.com>
2022-04-26 12:51:42 -0700

    working pokedex plus start of tests

---
+++ src/Views/Main/Main.Test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('pokeList', () => {
+  it('renders a list of characters to the screen that are filterable', async () => {
+    render;
+  });
+});
